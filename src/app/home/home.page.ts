9ee1bc97613d13a212a178a2f3b737256c862e55
Warit Santaputra <waritsan@gmail.com>
2019-08-05 15:41:00 +0700

    Add new data to line chart

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -100,25 +100,15 @@ export class HomePage implements OnInit {
         datasets: [
           {
             label: 'Sell per week',
-            fill: false,
-            lineTension: 0.1,
             backgroundColor: 'rgba(75,192,192,0.4)',
             borderColor: 'rgba(75,192,192,1)',
-            borderCapStyle: 'butt',
-            borderDash: [],
-            borderDashOffset: 0.0,
-            borderJoinStyle: 'miter',
-            pointBorderColor: 'rgba(75,192,192,1)',
-            pointBackgroundColor: '#fff',
-            pointBorderWidth: 1,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-            pointHoverBorderColor: 'rgba(220,220,220,1)',
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
             data: [65, 59, 80, 81, 56, 55, 40, 10, 5, 50, 10, 15],
-            spanGaps: false,
+          },
+          {
+            label: 'Buy per week',
+            backgroundColor: 'rgba(0,0,192,0.4)',
+            borderColor: 'rgba(0,0,192,1)',
+            data: [12, 65, 40, 2, 12, 90, 60, 50, 70, 30, 20, 80],
           }
         ]
       }
